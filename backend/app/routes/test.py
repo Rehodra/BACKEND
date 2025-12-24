28126f884871ba8f53ecd050eec2f712738d5c6e
Rehodra <mounasuvra@gmail.com>
2025-12-24 14:30:26 +0530

    deleted

--- backend/app/routes/test.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from app.db.mongodb import db
-
-router = APIRouter()
-
-@router.get("/db-test")
-async def db_test():
-    collections = await db.list_collection_names()
-    return {"collections": collections}
